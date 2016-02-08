c125a6d38e516d68f325e78e119dab550cd2e9bb
Junjie <jguo1495@sydney.edu.au>
2016-02-09 10:22:20 +1100

    First Upload

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Google Map Javascript API Test</title>
+    @yield("header")
+</head>
+<body>
+
+<div class="container">
+    @yield("content")
+</div>
+
+<!-- Build jQuery -->
+<!-- Build Stripe -->
+
+@yield("footer")
+
+</body>
+</html>
